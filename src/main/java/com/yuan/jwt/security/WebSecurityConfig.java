2a3ecfdb4b8ec8a0b0d01912c7202e894edae94c
mac <1751632917@qq.com>
2019-01-30 16:52:58 +0800

    1.添加WebSecurityConfig注释

--- src/main/java/com/yuan/jwt/security/WebSecurityConfig.java
+++ src/main/java/com/yuan/jwt/security/WebSecurityConfig.java
@@ -1,11 +1,16 @@
 package com.yuan.jwt.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author: mac
@@ -30,12 +35,33 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         // Entry points
         http.authorizeRequests()
                 .antMatchers(HttpMethod.OPTIONS).permitAll() //配合@CrossOrigin 解决跨域问题
+                .antMatchers("/users/signin").permitAll()//开放登录
+                .antMatchers("/users/signup").permitAll()//开发注册
                 .antMatchers("/test/**").permitAll() // test/**无需websecurity认证
                 // Disallow everything else..
                 .anyRequest().authenticated(); // 其他请求都需要认证
 
+        // 所有请求都将被JwtTokenFilterConfigurer拦截
         http.apply(new JwtTokenFilterConfigurer(jwtTokenProvider));
 
+    }
+
+    /**
+     * BCrypt加密类
+     * @return
+     */
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    /*
+     * 注入AuthenticationManager类
+     */
+    @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
 }
